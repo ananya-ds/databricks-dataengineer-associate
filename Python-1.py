c30565b2a6dd98dc2a57350b1d5ba54a1e062b12
ananya-ds <ananya-ds>
2023-03-15 07:02:09 +0000

    ds

---
+++ Python-1.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+print(1+2)
+
+# COMMAND ----------
+
+df  = spark.range(1,100)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+df.withColumn("sqrt", round(sqrt(col("id")),2)).show()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "I am a Data Scientist"
+
+# COMMAND ----------
+
+
